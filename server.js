5ecd3b957c96f41167a5cd1338682414322f9c36
kishore-devaraj <kishoregrylls@gmail.com>
2018-08-10 12:16:01 +0530

    Setup default static server in express js

--- server.js
+++ server.js
@@ -2,8 +2,18 @@ const express = require('express')
 
 const app = express()
 
+// Middlewares
+app.use(express.static(__dirname + '/public')) // To Deliver static assets and contents
+
 app.get('/', (req, res) => {
   res.send('<h4>Hello Express</h4>')
+})
+
+app.get('/json', (req, res) => {
+  res.send({
+    'name': 'kishore',
+    'career': 'full stack web developer'
+  })
 })
 
 app.listen(3000, () => {
