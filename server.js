55d098f6706611117b1ba266113edb42375209be
kishore-devaraj <kishoregrylls@gmail.com>
2018-08-10 12:59:41 +0530

    HandleBars is set as a view engine for express

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
+
+app.set('viewengine','hbs')
 
 // Middlewares
 app.use(express.static(__dirname + '/public')) // To Deliver static assets and contents
 
+
 app.get('/', (req, res) => {
-  res.send('<h4>Hello Express</h4>')
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    currentYear: new Date().getFullYear(),
+    welcomeMessage: 'Welcome to my Page'
+  })
 })
 
-app.get('/json', (req, res) => {
-  res.send({
-    'name': 'kishore',
-    'career': 'full stack web developer'
+app.get('/about', (req, res) => {
+  res.render('about.hbs',{
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
   })
 })
 
